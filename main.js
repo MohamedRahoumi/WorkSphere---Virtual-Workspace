1fb0e912567dd41793cf02d91b3c386f1115a93c
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-11-13 10:18:04 +0100

    open et ferme le formulaire

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ const openForm = document.getElementById("openForm");
 const closeForm = document.getElementById("closeForm");
 const popup = document.getElementById("popup");
 
+openForm.onclick = () => popup.classList.remove("hidden");
+
+closeForm.onclick = () => {
+  popup.classList.add("hidden");
+  
+};
